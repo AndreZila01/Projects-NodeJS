602125076ece27f2b59ba2ea5892ba6b1e0a6259
André Custódio <andrecustodiotablet@gmail.com>
2024-10-12 11:34:03 +0100

    Atualizacao no codigo de Marca de Agua

--- Criar Marca de Agua/index.js
+++ Criar Marca de Agua/index.js
@@ -1,35 +1,59 @@
-const { Jimp } = require('jimp');
+const { Jimp } = require('jimp');//npm install jimp jimp@0.22.10
 let fs = require('fs');
+
+let content = "© Andrezila1"
 
 async function load() {
 
-    if (!fs.existsSync("./MarcaHelpH.png" || "./MarcaHelpV.png")) {
-        if (!fs.existsSync("./FicheirosS"))
-            fs.mkdirSync("./FicheirosS");
-        if (!fs.existsSync("./FicheirosC"))
-            fs.mkdirSync("./FicheirosC");
+    // if (!fs.existsSync("./MarcaHelpH.png" || "./MarcaHelpV.png")) {
+    //     if (!fs.existsSync("./FicheirosS"))
+    //         fs.mkdirSync("./FicheirosS");
+    //     if (!fs.existsSync("./FicheirosC"))
+    //         fs.mkdirSync("./FicheirosC");
 
-        await Jimp.read('./MarcaHelpH.png').then(async (MarcaH) => {
-            await Jimp.read('./MarcaHelpV.png').then(async (MarcaV) => {
-                fs.readdir('./FicheirosS', async (err, data) => {
-                    let count = 0;
-                    data.forEach(async file => {
-                        await Jimp.read(`./FicheirosS/${file}`).then(async (fir_img) => {
-                            if (fir_img.height < fir_img.width)
-                                fir_img.blit(MarcaH, 0, 0);
-                            else
-                                fir_img.blit(MarcaV, 0, 0);
+    //     await Jimp.read('./MarcaHelpH.png').then(async (MarcaH) => {
+    //         await Jimp.read('./MarcaHelpV.png').then(async (MarcaV) => {
+    //             fs.readdir('./FicheirosS', async (err, data) => {
+    //                 let count = 0;
+    //                 data.forEach(async file => {
+    //                     await Jimp.read(`./FicheirosS/${file}`).then(async (fir_img) => {
+    //                         if (fir_img.height < fir_img.width)
+    //                             fir_img.blit(MarcaH, 0, 0);
+    //                         else
+    //                             fir_img.blit(MarcaV, 0, 0);
 
 
-                            fir_img.write(`./FicheirosC/${count++}.JPG`);
-                        });
+    //                         fir_img.write(`./FicheirosC/${count++}.JPG`);
+    //                     });
+    //                 });
+    //             });
+    //         });
+    //     });
+    // }
+    // else
+    //     console.log("Falta o ficheiro de marca de água! Por favor ... crie a sua marca de agua");
+
+    if (!fs.existsSync("./FicheirosS"))
+        fs.mkdirSync("./FicheirosS");
+    if (!fs.existsSync("./FicheirosC"))
+        fs.mkdirSync("./FicheirosC");
+
+    await Jimp.loadFont(Jimp.FONT_SANS_128_WHITE).then(async (font) => {
+        fs.readdir('./FicheirosS', async (err, data) => {
+            let count = 0;
+            data.forEach(async file => {
+                if (file.toLowerCase().includes(".jpg") || file.toLowerCase().includes(".jpeg") || file.toLowerCase().includes(".png") || file.toLowerCase().includes(".bmp") || file.toLowerCase().includes(".tiff") || file.toLowerCase().includes(".webp") || file.toLowerCase().includes(".svg"))
+                    await Jimp.read(`./FicheirosS/${file}`).then(async (fir_img) => {
+
+                        //fir_img.blur(15);
+                        fir_img.print(font, 75, (fir_img.bitmap.height - 200), content);
+
+                        await new Promise(resolve => setTimeout(resolve, 1500));
+                        await fir_img.write(`./FicheirosC/${count++}.JPG`);
                     });
-                });
             });
         });
-    }
-    else
-        console.log("Falta o ficheiro de marca de água! Por favor ... crie a sua marca de agua");
+    });
 }
 
 load();
